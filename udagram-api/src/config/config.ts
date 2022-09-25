3b6e573660d0997ae16c63b9724db9cc8cc13ef9
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-09-25 08:50:35 +0300

    Initial installations

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': process.env.POSTGRES_USERNAME||"postgres",
+  'password': process.env.POSTGRES_PASSWORD||"udapeople12",
+  'database': process.env.POSTGRES_DB||"postgres",
+  'host': process.env.POSTGRES_HOST||"udapeople.cngwfvqe9imi.us-east-1.rds.amazonaws.com",
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': process.env.AWS_REGION||"us-east-1",
+  'aws_profile': process.env.AWS_PROFILE||"udacity-c3-user",
+  'aws_media_bucket': process.env.AWS_BUCKET||"arn:aws:s3:::photobucket-28981100",
+  'url': process.env.URL||"http://localhost:8100",
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': process.env.JWT_SECRET||"testing",
   },
 };
